34d27f04316d5715a583463dfa040b6d89037e0a
Zmaplex <Zmaplex@gmail.com>
2019-05-07 17:55:16 +0800

    增加 MVP 基类，为接下来使用 Dagger2 全面改造项目做准备。

---
+++ common/src/main/java/com/zmaplex/common/BaseView.java
@@ -0,0 +1,8 @@
+package com.zmaplex.common;
+
+/**
+ * @author SmartSean Created on 2018/1/25 16:19.
+ */
+
+public interface BaseView {
+}
---
+++ common/src/main/java/com/zmaplex/common/IModel.java
@@ -0,0 +1,9 @@
+package com.zmaplex.common;
+
+/**
+ * @author SmartSean Created on 2018/1/25 16:20.
+ */
+
+public interface IModel {
+    void onDestroy();
+}
